f16c6124b36c9993a5d28d8c3d9574e60ecd625a
Holinda Beshiri <holinda_b@hotmail.com>
2024-05-06 17:19:02 +0000

    Add steps for deployment to Heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-hob=07is7jo5rll_px^3et%9%a$^e^zx@%@_4j0p$cs#$eag)f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-lin2907-ciaobellablog-5vqh9e7nikk.ws-eu111.gitpod.io']
+ALLOWED_HOSTS = ['8000-lin2907-ciaobellablog-5vqh9e7nikk.ws-eu111.gitpod.io',
+                 '.herokuapp.com']
 
 
 # Application definition
