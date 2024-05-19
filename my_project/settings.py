8c7c0c415392f8a576c08b8dcf0a97ac7017439f
Holinda Beshiri <holinda_b@hotmail.com>
2024-05-19 14:45:36 +0000

    Embedded styles in blog post list, due to overlapping

--- my_project/settings.py
+++ my_project/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '8000-lin2907-ciaobellablog-p9n8fzp3bj1.ws-eu114.gitpod.io',
                   '.herokuapp.com']
