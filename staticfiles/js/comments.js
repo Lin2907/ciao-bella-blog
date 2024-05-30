ee4feb9ecaaa218b1f46cd61a518478f271f6f56
Holinda Beshiri <holinda_b@hotmail.com>
2024-05-30 18:46:30 +0000

    Update delete comment option, fix the post author and date not showing

--- staticfiles/js/comments.js
+++ staticfiles/js/comments.js
@@ -1,6 +1,6 @@
 const editButtons = document.getElementsByClassName("btn-edit");
 const commentText = document.getElementById("id_body");
-const commentForm = document.getElementById("commentForm");
+const commentForm = document.getElementById("recipeCommentForm");
 const submitButton = document.getElementById("submitButton");
 
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
@@ -8,23 +8,20 @@ const deleteButtons = document.getElementsByClassName("btn-delete");
 const deleteConfirm = document.getElementById("deleteConfirm");
 
 
-
 for (let button of editButtons) {
-    button.addEventListener("click", (e) => {
-        let commentId = e.target.getAttribute("comment_id");
-        let commentContent = document.getElementById(`comment${commentId}`).innerText;
-        commentText.value = commentContent;
-        submitButton.innerText = "Update";
-        commentForm.setAttribute("action", `edit_comment/${commentId}`);
-    });
+  button.addEventListener("click", (e) => {
+    let commentId = e.target.getAttribute("comment_id");
+    let commentContent = document.getElementById(`comment${commentId}`).innerText;
+    commentText.value = commentContent;
+    submitButton.innerText = "Update";
+    commentForm.setAttribute("action", `edit_comment/${commentId}`);
+  });
 }
 
-
- for (let button of deleteButtons) {
-    button.addEventListener("click", (e) => {
-        let commentId = e.target.getAttribute("comment_id");
-        deleteConfirm.href = `delete_comment/${commentId}`;
-        deleteModal.show();
-    });
+for (let button of deleteButtons) {
+  button.addEventListener("click", (e) => {
+    let commentId = e.target.getAttribute("comment_id");
+    deleteConfirm.href = `delete_comment/${commentId}`;
+    deleteModal.show();
+  });
 }
-
